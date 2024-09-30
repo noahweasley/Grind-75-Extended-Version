376ceca0e7aa2910725d9666dd8fc6f8e683da9b
noahweasley <iebenmelu@gmail.com>
2024-09-30 14:05:31 -0700

    Update project configuration

---
+++ solution/src/easy/LCABinaryTree.java
@@ -0,0 +1,44 @@
+package easy;
+
+import util.TreeNode;
+
+class LCABinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.buildTree(new int[]{2, 1});
+        TreeNode p = TreeNode.buildTree(new int[]{2});
+        TreeNode q = TreeNode.buildTree(new int[]{1});
+
+        System.out.println(lowestCommonAncestor(root, p, q));
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if ((root.val == p.val && root.val == q.val)) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        } else if (left == null && right != null) {
+            return right;
+        } else if (right == null && left != null) {
+            return left;
+        }
+
+        boolean isInLeftNode = left == null || left.val == p.val || left.val == q.val;
+        boolean isInRightNode = right == null || right.val == p.val || right.val == q.val;
+
+        if (isInLeftNode && isInRightNode) {
+            return root;
+        }
+
+        return null;
+    }
+}
