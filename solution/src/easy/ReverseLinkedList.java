0e0a898c1db3259ee2c752353c8337d7b722cf34
noahweasley <iebenmelu@gmail.com>
2024-10-04 04:33:46 -0700

    Add reverse linked list solution

---
+++ solution/src/easy/ReverseLinkedList.java
@@ -0,0 +1,25 @@
+package easy;
+
+import util.ListNode;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode node = ListNode.buildFromList(new int[]{1, 2, 3});
+        System.out.println(reverseList(node));
+    }
+
+    private static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
