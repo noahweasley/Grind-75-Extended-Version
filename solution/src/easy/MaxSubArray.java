f2e89c5a87a7596cc757020227cf80e68149085b
noahweasley <iebenmelu@gmail.com>
2024-11-04 11:54:22 +0100

    Add Max sub-array sum solution using Kadane's Algorithm

---
+++ solution/src/easy/MaxSubArray.java
@@ -0,0 +1,22 @@
+package easy;
+
+public class MaxSubArray {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, -8, 7, -1, 2, 3};
+        System.out.println(maxSubArray(arr));
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int windowSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            windowSum = Math.max(windowSum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+}
+
